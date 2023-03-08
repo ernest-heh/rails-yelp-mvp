62ba9a33046f3d4545717b59c60478a9def0e2e1
Ernest Ho <ernest.heh@gmail.com>
2023-03-08 12:59:15 +0800

    added page to display all restaurants

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.create!(
+  name: 'Claudine',
+  address: '39C Harding Road',
+  phone_number: '249541',
+  category: 'french'
+)
+Restaurant.create!(
+  name: 'Oumi',
+  address: '51-01, CapitaSpring, 88 Market Street',
+  phone_number: '048948',
+  category: 'japanese'
+)
+Restaurant.create!(
+  name: 'Min Jiang at Dempsey',
+  address: '7A & 7B Dempsey Road',
+  phone_number: '249684',
+  category: 'chinese'
+)
+Restaurant.create!(
+  name: 'Cicheti',
+  address: '52 Kandahar Street',
+  phone_number: '198901',
+  category: 'italian'
+)
+Restaurant.create!(
+  name: 'Kingdom of Belgians',
+  address: '8 Rodyk Street',
+  phone_number: '6634 0500',
+  category: 'belgian'
+)
